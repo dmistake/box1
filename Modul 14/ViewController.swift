cc899816378338ba57c0d121f7f3b0d439c77dc8
Александр Головин <alexander@MacBook-Pro-Aleksandr.local>
2021-03-01 13:52:24 +0300

    first commit

---
+++ Modul 14/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Modul 14
+//
+//  Created by Александр Головин on 01.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
